15a3389ec178eb234b06b391dda61a75038fe9ed
hwacdx <1296324895@qq.com>
2024-10-28 23:36:44 +0800

    修复运行 mysql.py 报错

---
+++ mysql2.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+
+# 运行时报错 ModuleNotFoundError: No module named 'mysql.connector'; 'mysql' is not a package
+
+# 解决：将文件名 mysql.py 修改为其他名称即可。因为系统自带了 mysql 模块。
+
+# 参考链接：https://blog.csdn.net/lijinshanba/article/details/85337448
+
+
+
+
